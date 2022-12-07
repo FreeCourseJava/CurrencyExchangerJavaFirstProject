86c125f8ba9d57ec94cc90a3dbf8b178ba8bf6e1
neznaika38 <neznaika_38@mail.ru>
2022-12-07 17:37:08 +0300

    * 1. added 2 new Classes (1 of them is Victor's)   2. made 2 methods for receive Currency

---
+++ src/main/java/com/example/service/CurrencyList.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.entity.Currency;
+
+import java.util.List;
+
+public interface CurrencyList {
+    public List<Currency> getCurrencyRates();
+}
---
+++ src/main/java/com/example/service/CurrencyListImpl.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import com.example.entity.Currency;
+
+import java.util.List;
+
+public class CurrencyListImpl  implements CurrencyList{
+    @Override
+    public List<Currency> getCurrencyRates() {
+      return null;
+    }
+}
